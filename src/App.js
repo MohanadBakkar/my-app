6afb9cac0708bdac8b86db8ec904f743d8c8f3d3
mohanad <mohanadbkar@hotmail.com>
2024-01-24 20:02:13 +0200

    adding react project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CustomCard from "./components/CustomCard";
+const App = () => {
+    return <>
+        <NavBar />
+        <HeroSection />
+        <CustomCard />
+        <CustomCard />
+        <CustomCard />
+    </>
 }
 
 export default App;
---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,5 @@
+const HeroSection = () => {
+    return <h1>this is a hero section</h1>
+}
+
+export default HeroSection
